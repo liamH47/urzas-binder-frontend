b9b3b5653327ecbbef515a0999eda63ad78e1a6d
liamH47 <liamhanafee@gmail.com>
2020-11-29 17:56:22 -0500

    barebones sign up and nav bar

---
+++ src/Components/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Signup extends React.Component {
+
+    render() {
+        return(
+            <p>Please sign up for this great app</p>
+        )
+    }
+}
+
+export default Signup
